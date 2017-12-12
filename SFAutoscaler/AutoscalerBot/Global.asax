2b6f75f316ec63f47467fd24665f641f67e4d819
Lynn Orrell <lyorrell@microsoft.com>
2017-12-12 11:59:35 -0800

    Added bot projects

---
+++ SFAutoscaler/AutoscalerBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoscalerBot.WebApiApplication" Language="C#" %>
---
+++ SFAutoscaler/AutoscalerBotService/IOwinAppBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Owin;
+
+namespace AutoscalerBotService
+{
+    public interface IOwinAppBuilder
+    {
+        void Configuration(IAppBuilder appBuilder);
+    }
+}
