924c6cc128de6e0a1a034a3ddbde60b98866eca8
Matthew Snider <masnider@microsoft.com>
2017-12-12 11:44:49 -0800

    update to load service and startup

---
+++ SFAutoscaler/SFAutoscalerApplication/Scripts/CreateLoadService.ps1
@@ -0,0 +1,8 @@
+﻿$i = 0
+$target = 2
+
+while($i -lt $target)
+{
+    New-ServiceFabricService -ServiceTypeName LoadGeneratorServiceType -Stateless -ApplicationName "fabric:/SFAutoscalerApplication" -ServiceName "fabric:/SFAutoscalerApplication/LoadGen$i" -PartitionSchemeSingleton -InstanceCount 1 -ServicePackageActivationMode ExclusiveProcess
+    $i++;
+}
