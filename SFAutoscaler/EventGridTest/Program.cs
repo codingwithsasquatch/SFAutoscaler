34519f9760dc7bea170477527143b28c45117dd5
David Barkol <dbarkol-c@convercent.com>
2017-12-12 12:21:01 -0800

    Added console app event grid test

---
+++ SFAutoscaler/EventGridTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventGrid.Helper;
+using EventGrid.Helper.Events;
+
+namespace EventGridTest
+{
+    class Program
+    {
+        private const string TopicEndpoint = "[place endpoint here]";
+        private const string TopicKey = "[place key here]";
+
+        static void Main(string[] args)
+        {
+            SendLoadInfo().Wait();
+            Console.WriteLine("Press <enter> to exit.");
+            Console.ReadLine();
+        }
+
+        private static async Task SendLoadInfo()
+        {
+            var errors = new List<GridEvent<LoadInfo>>
+            {
+                new GridEvent<LoadInfo>()
+                {
+                    Data =
+                        new LoadInfo()
+                        {
+                            Description = "Something"
+                        },
+                    Subject = "clusterevent/loadinfo",
+                    EventType = "loadinfo",
+                    EventTime = DateTime.UtcNow,
+                    Id = Guid.NewGuid().ToString()
+                }
+            };
+
+            await Utils.SendEvent(TopicEndpoint, TopicKey, errors);
+        }
+    }
+}
