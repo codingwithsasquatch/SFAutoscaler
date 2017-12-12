28acebe018a4611407b723244585acf6c545a860
Matthew Snider <masnider@microsoft.com>
2017-12-12 14:03:09 -0800

    adding monitor and reporting

--- SFAutoscaler/EventGrid.Helper/Events/LoadInfo.cs
+++ SFAutoscaler/EventGrid.Helper/Events/LoadInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Fabric.Query;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,6 @@ namespace EventGrid.Helper.Events
 {
     public class LoadInfo
     {
-        public string Description { get; set; }
+        public Dictionary<string, LoadMetricInformation> ClusterLoadInfo { get; set; }
     }
 }
--- SFAutoscaler/LoadMonitorService/LoadMonitorService.cs
+++ SFAutoscaler/LoadMonitorService/LoadMonitorService.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using EventGrid.Helper;
+using EventGrid.Helper.Events;
 
 namespace LoadMonitorService
 {
@@ -15,53 +17,59 @@ namespace LoadMonitorService
     /// </summary>
     internal sealed class LoadMonitorService : StatefulService
     {
+        private const string TopicEndpoint = "https://sfautoscaler.westus2-1.eventgrid.azure.net/api/events";
+        private const string TopicKey = "tWtFrgaD70han/OQKCrhawJuYmvEG92Wr02RORbJ1Fc=";
+        private static FabricClient fc = new FabricClient();
+
         public LoadMonitorService(StatefulServiceContext context)
             : base(context)
         { }
 
-        /// <summary>
-        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
-        /// </summary>
-        /// <remarks>
-        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
-        /// </remarks>
-        /// <returns>A collection of listeners.</returns>
-        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
-        {
-            return new ServiceReplicaListener[0];
-        }
 
-        /// <summary>
-        /// This is the main entry point for your service replica.
-        /// This method executes when this replica of your service becomes primary and has write status.
-        /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
             var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
 
-            while (true)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
+                var loadInfo = (await fc.QueryManager.GetClusterLoadInformationAsync()).LoadMetricInformationList.ToDictionary((info) => info.Name);
+                
+                //generate the new message ID
                 using (var tx = this.StateManager.CreateTransaction())
                 {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+                    var result = await myDictionary.TryGetValueAsync(tx, "Counter", LockMode.Update);
 
                     ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
                         result.HasValue ? result.Value.ToString() : "Value does not exist.");
 
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
+                    var messageId = await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
 
                     // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
                     // discarded, and nothing is saved to the secondary replicas.
+
+                    var loadInfoEvents = new List<GridEvent<LoadInfo>>
+                    {
+                            new GridEvent<LoadInfo>()
+                            {
+                                Data =
+                                    new LoadInfo()
+                                    {
+                                        ClusterLoadInfo = loadInfo
+                                    },
+                                Subject = "clusterevent/loadinfo",
+                                EventType = "loadinfo",
+                                EventTime = DateTime.UtcNow,
+                                Id = messageId.ToString()
+                            }
+                    };
+
+                    await Utils.SendEvent(TopicEndpoint, TopicKey, loadInfoEvents);
+
                     await tx.CommitAsync();
                 }
 
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
             }
         }
     }
