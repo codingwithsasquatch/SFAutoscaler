aaa3a9e56040408d2c82228b0c7a3b685f2fbb98
David Barkol <dbarkol-c@convercent.com>
2017-12-12 11:49:31 -0800

    Added Event Grid helper library with models for load and scale events.

---
+++ SFAutoscaler/EventGrid.Helper/GridEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventGrid.Helper
+{
+    public class GridEvent<T> where T : class
+    {
+
+        public string Id { get; set; }
+
+
+        public string Subject { get; set; }
+
+
+        public string EventType { get; set; }
+
+
+        public T Data { get; set; }
+
+
+        public DateTime EventTime { get; set; }
+
+    }
+}
